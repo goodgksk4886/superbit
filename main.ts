2c2ce3cfe9a84e5b623e68fb80b9999cf6e45fd3
goodgksk4886 <84364731+goodgksk4886@users.noreply.github.com>
2022-06-10 03:41:10 +0900

    Main Frame

--- main.ts
+++ main.ts
@@ -1,4 +1,38 @@
-basic.showIcon(IconNames.Heart)
-basic.forever(function () {
-	
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Left") {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        0,
+        SuperBit.enMotors.M3,
+        255
+        )
+    } else if (receivedString == "Right") {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        255,
+        SuperBit.enMotors.M3,
+        0
+        )
+    } else if (receivedString == "Forward") {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        255,
+        SuperBit.enMotors.M3,
+        255
+        )
+    } else if (receivedString == "Backward") {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        -100,
+        SuperBit.enMotors.M3,
+        -100
+        )
+    } else {
+        SuperBit.MotorStopAll()
+    }
 })
+radio.setGroup(1)
+let strip = neopixel.create(DigitalPin.P4, 4, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.showIcon(IconNames.Happy)
